4e798ab3d080ac4c3e0cf5d5f45dcf0c20909927
Jeremy <010JGL@GMAIL.COM>
2023-05-03 23:11:51 -0400

    add page showing category and products total

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_BASIC_USER'], password: ENV['HTTP_BASIC_PASSWORD']
 
   def show
+    #get the values from the database and assign to value here, pass it to the template
+    @products_total = Product.count
+    @categories_total = Category.count
   end
 end
